4b876e968d43440bb92de2ccac3b136c885b808f
Mauricio Maroto <maulberto3@hotmail.com>
2024-07-07 21:16:51 -0600

    init

---
+++ benches/distance.rs
@@ -0,0 +1,15 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use pca_bench::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("distance", |bench| {
+        bench.iter(|| {
+            let a = Vector2::new(0.0, 0.0);
+            let b = Vector2::new(3.0, 4.0);
+            a.distance_squared(&b);
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
